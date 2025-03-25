ccffe47d0f5d5729ff6e1e421e64774d76b687fd
kartik biradar <biradarkartik690@gmail.com>
2025-03-25 09:20:17 +0530

    first working version without edit rout day 1

---
+++ app.js
@@ -0,0 +1,77 @@
+// basic template
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const methoodOverride = require("method-override");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methoodOverride("_method"));
+
+
+// calling mongooes models
+const listings = require("./models/listings.js");
+
+// calling mongoose to connect server
+function mongoCall() {
+  try {
+    mongoose.connect("mongodb://127.0.0.1:27017/AIRBNB");
+  } catch (err) {
+    console.log(
+      "Error in mongoCall function while connectiong mongoose server"
+    );
+  }
+}
+mongoCall();
+
+
+
+// routs
+
+app.get("/", async (req, res) => {
+  console.log("HIT '/'");
+  let data = await listings.find({});
+  // console.log(data)
+  res.render("./listings/index", { data });
+});
+
+// show rout
+app.get("/:id", async (req, res) => {
+  console.log("HIT '/show'");
+  let data = await listings.findById(req.params.id);
+  res.render("./listings/show", { data });
+});
+
+// adding data
+app.get("/listings/create", (req, res) => {
+  res.render("./listings/new");
+});
+
+app.post("/listings/create", async (req, res) => {
+  console.log(req.body.listings);
+  let data = listings(req.body.listings);
+  data.save();  
+  res.redirect("/");
+});
+
+// editing data
+app.get("/listings/:id/edit", async (req, res) => {
+  console.log("HIT : /listings:id/edit");
+  let data = await listings.findById(req.params.id);
+  res.render("./listings/edit", { data });
+});
+
+
+
+
+  
+// trigring server
+app.listen(3000, (req, res) => {
+  console.log("server is running on port 3000");
+});   
+     
---
+++ models/listings.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+        title: {
+            type: String,
+            required: true,
+        },
+        description: String,
+        image: {
+            type: String,
+            default:
+            "https://marketplace.canva.com/EAGYna2-QZ8/1/0/1600w/canva-brown-and-black-minimalist-hotel-presentation-SqQPhkMUHTQ.jpg",
+            set: (v) =>
+            v == ""
+                ? "https://marketplace.canva.com/EAGYna2-QZ8/1/0/1600w/canva-brown-and-black-minimalist-hotel-presentation-SqQPhkMUHTQ.jpg"
+                : v,
+        },
+        price: String,
+        location: String,
+        country: String,  
+});
+
+console.log("module")
+module.exports = mongoose.model("listings",schema)
